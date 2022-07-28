a7d1c5a3cb0680b5aa7626032dd6009667165b27
Coder <coder@foo.com>
2022-07-28 13:17:27 +0530

    Remove duplicates - efficient solution

--- src/com/leetcode/practice/array/RemoveDuplicatesDriver.java
+++ src/com/leetcode/practice/array/RemoveDuplicatesDriver.java
@@ -22,84 +22,16 @@ public class RemoveDuplicatesDriver {
     }
 }
 
-/*
-Approach :
-
- Sub-tasks :
-  - Find duplicates - DONE
-  - remove duplicates (replace with values) - DONE
-  - re-arrange the values - WIP
-
- -----------------
-  1. Find duplicates (use 2 pointers)
-      - Since it's sorted it'll be easy & adjacent elements will be duplicates
-      - Pickup the 1st element (1st pointer)
-      - Move the second pointer
-      - if 2nd pointer has same elements -
-      - replace it with a flag value (lyk underscore)
-      - increase the duplicate count
-
-  2. Re-arrange values :
-      - Do another iteration (after identifying duplicates)
-      - Now, start with where there's a duplicate - 1st pointer
-      - Move 2nd pointer
-      - Find where there's an integer.
-      - Swap those two elements.
-      - Move 1st pointer to the next index of 2nd pointer
-
-      Note : Handle the index overflow
- */
 class DuplicateRemover {
     public int removeDuplicates(int[] nums) {
 
-        if(nums.length == 1){
-            return 0;
+        int index = nums.length > 0 ? 1 : 0;
+
+        for(int currentElement : nums) {
+          if(currentElement > nums[index - 1])  {
+              nums[index++] = currentElement;
+          }
         }
-
-        int duplicateCount = replaceDuplicateValues(nums);
-        int start = 0;
-        int end = 1;
-
-        while(start < nums.length - 1 && end < nums.length){
-            if(nums[start] == -1000 && nums[end] != -1000){
-                swap(nums, start, end);
-                start++;
-                end ++;
-            }
-            else if(nums[start] != -1000){
-                start++;
-                end++;
-            }
-            else if(nums[end] == -1000){
-                end++;
-            }
-        }
-
-        return duplicateCount;
-    }
-
-    private void swap(int[] nums, int start, int end) {
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-    }
-
-
-    private int replaceDuplicateValues(int[] nums) {
-        int start = 0;
-        int end = 1;
-        int duplicateCount = 0;
-        while(start < nums.length - 1 && end < nums.length){
-            if(nums[start] != -1000 && nums[start] == nums[end] ){
-                nums[end] = -1000;
-                end++;
-                duplicateCount++;
-            }
-            else if (nums[start] != nums[end]){
-                start = end;
-                end++;
-            }
-        }
-        return duplicateCount;
+        return index;
     }
 }
