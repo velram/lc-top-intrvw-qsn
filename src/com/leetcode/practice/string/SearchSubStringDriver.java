4e97667db674a24a35c4ef4b86c44b6f00a7bb2f
Coder <coder@foo.com>
2022-08-17 22:19:58 +0530

    Vel - sample commit

--- src/com/leetcode/practice/string/SearchSubStringDriver.java
+++ src/com/leetcode/practice/string/SearchSubStringDriver.java
@@ -7,6 +7,7 @@ package com.leetcode.practice.string;
 public class SearchSubStringDriver {
     public static void main(String[] args) {
 
+        System.out.println("Testing Sub string search ");
     }
 }
 
